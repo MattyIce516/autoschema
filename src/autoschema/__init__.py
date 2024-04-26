fef8642bc1e0db9d3220c7e7dc98dec102bf4e3b
Matt <mattlax516@gmail.com>
2024-04-26 03:09:37 -0400

    Updating docs in __init__

--- src/autoschema/__init__.py
+++ src/autoschema/__init__.py
@@ -1,4 +1,24 @@
-# Package
+"""
+Available definitions:
+
+1. read_universal(loc: str, rename: Union[dict, None] = None, **kwargs) -> DataFrame
+2. standardize_dataframe_columns(df: pd.DataFrame, inplace: bool = True) -> pd.DataFrame
+3. auto_schema(
+        df: pd.DataFrame, 
+        standardize_names: bool = True,
+        write_schema: bool = True,
+        schema_file_name: Union[str, None] = None
+    ) -> pd.DataFrame
+
+Available Classes:
+
+1. SchemaValidator
+    Methods:
+        a. fit(X)
+        b. transform(X)
+        c. fit_transform(X)
+"""
+
 from .util import read_universal, standardize_dataframe_columns
 from .autoschema import auto_schema, SchemaValidator
 
