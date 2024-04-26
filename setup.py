33027d3dea5521e0452adc9f3733a1189ec105dd
Matt <mattlax516@gmail.com>
2024-04-26 03:26:32 -0400

    Everything working again

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 import os
 from setuptools import setup, find_packages
 
-packages = find_packages(where='src')
-print("Found packages:", packages)
-
-
 with open("README.md", "rt", encoding="utf-8") as fh:
     long_description = fh.read()
 
